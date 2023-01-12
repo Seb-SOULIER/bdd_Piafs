4031148ad7605f229e02280de975cc28be892b90
Seb-SOULIER <soulier.sebastien.ss@gmail.com>
2023-01-12 12:05:00 +0100

    inscritpion

--- src/Controller/AtelierController.php
+++ src/Controller/AtelierController.php
@@ -12,6 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Date;
 
 class AtelierController extends AbstractController
 {
@@ -231,6 +232,15 @@ class AtelierController extends AbstractController
                     ]]
                 ]);
             }
+        }
+
+        if($reservationSend === []){
+            array_push($reservationSend, [
+                'title'=> new DateTime('now'),
+                'data'=> [[
+                    "atelier"=>"Pas d'inscription a un atelier",
+                ]]
+            ]);
         }
         
         return $this->json([
