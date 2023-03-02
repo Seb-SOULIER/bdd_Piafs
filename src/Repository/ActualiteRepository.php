6825ae0bcf04f097b7264c0749c260870e5fc86e
Seb-SOULIER <soulier.sebastien.ss@gmail.com>
2023-03-02 13:24:02 +0100

    actualite delete

--- src/Repository/ActualiteRepository.php
+++ src/Repository/ActualiteRepository.php
@@ -45,7 +45,8 @@ class ActualiteRepository extends ServiceEntityRepository
     public function find3last(): array
     {
         return $this->createQueryBuilder('a')
-            ->andWhere('actived = true')
+            ->andWhere('actived = :val')
+            ->setParameter('val', true)
             ->orderBy('a.DateAt','DESC')
             ->setMaxResults(3)
             ->getQuery()
